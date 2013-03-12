ba1cc1babf927473dcf20634d3632cf891ff4214
Maria Johnson <m9bernadette@gmail.com>
2013-03-12 14:51:05 -0600

    TestCommit

---
+++ github_helloworld.py
@@ -0,0 +1,20 @@
+import turtle
+
+turtle.showturtle()
+turtle.pensize(5)
+turtle.color("blue")
+turtle.circle(100)
+turtle.penup()
+turtle.goto(0, 20)
+turtle.pendown()
+turtle.color("green")
+turtle.circle(80)
+turtle.penup()
+turtle.goto(-25, 90)
+turtle.pendown()
+turtle.color("blue")
+turtle.write("Hello World")
+turtle.hideturtle()
+turtle.done()
+
+
